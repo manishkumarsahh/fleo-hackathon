1cdd48f0dca8330bb33d7ef00ca6464608370d56
Manish kumar sah <manishsah23121999@gmail.com>
2021-07-12 12:55:48 +0530

    major project

--- index.js
+++ index.js
@@ -1,19 +1,25 @@
 const express = require('express');
+
+
+const port = process.env.PORT || 8000;
 const app = express();
-const port =process.env.PORT|| 8000;
-
-
-
-app.set('view engine','ejs');
-app.set('views', './views');
 
 app.use('/', require('./routes'));
 
 
+app.set('view engine','ejs');
+app.set('views', './views');
+app.use(express.urlencoded({extended:true}));
+app.use(express.static('assets'));
+
+ 
+
+
 app.listen(port,function(err){
     if(err){
-        console.log('error in running server');
+        console.log('error in running server ',err  );
     }
+    console.log('yup my server is running on port',port);
 
-    console.log('server is running on port:8000');
+
 });
